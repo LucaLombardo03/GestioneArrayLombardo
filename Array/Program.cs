5b6e4ce5f72bf22266f3de4071f740ef17aded12
luca lombardo <lucalombardo03@gmail.com>
2019-11-21 19:45:59 +0100

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LibreriaArray;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int [3];
+            GestioneArray.LeggiArray(array);
+            GestioneArray.StampaArray(array);
+            Console.ReadLine();
+        }
+    }
+}
